74a983f86630c3fffc9c2ef0e44d12286037a35b
jderbyshire <jderbyshire@acromedia.com>
2022-12-01 14:05:09 -0800

    added p2 ans

--- day1/sln.js
+++ day1/sln.js
@@ -14,10 +14,17 @@ const sln = (input) => {
     }
   }
 
+  // part 2 - return the sum of the top 3 elfs
+  const top3Sum = Object.values(elfsCalories)
+    .sort((a, b) => b - a)
+    .slice(0, 3)
+    .reduce((a, b) => a + b);
+
   const elf = Object.entries(elfsCalories).reduce((a, e) =>
     a[1] > e[1] ? a : e
   );
-  return `Elf ${elf[0]} has a total of ${elf[1]} calories`;
+
+  return `Elf ${elf[0]} has a total of ${elf[1]} calories. P2: sum of top 3 elves is ${top3Sum}`;
 };
 
 console.log(sln(jsonData));
